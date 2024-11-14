3a66a06a51d3c222a8fb7afcd0e9467907becfec
Abdur Rehman <claudethebitplayer@gmail.com>
2024-11-14 14:29:55 +0500

    Update DisplayGrid

--- src/components/DisplayGrid.jsx
+++ src/components/DisplayGrid.jsx
@@ -19,7 +19,7 @@ export default function DisplayGrid({ setScore, setScoreIncrement, score }) {
     {row: 15, col: 28}
   ])
   const [snakeDirection, setSnakeDirection] = useState("RIGHT");
-  const [moveSpeed, setMoveSpeed] = useState(100);
+  const [moveSpeed, setMoveSpeed] = useState(50);
 
   const [grid, setGrid] = useState([]);
   const [isFood, setIsFood] = useState(false);
@@ -54,7 +54,7 @@ export default function DisplayGrid({ setScore, setScoreIncrement, score }) {
       { row: 15, col: 28 }
     ])
     setSnakeDirection("RIGHT");
-    setMoveSpeed(100);
+    setMoveSpeed(50);
   
     setHitWall(false);
     setHitSelf(false);
